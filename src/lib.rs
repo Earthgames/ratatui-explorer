2fd0ed087d6c1818f6a2ff9484b7c78f6eb66f12
Earthgames <earthgame_s@outlook.com>
2025-02-07 18:21:32 +0100

    NO warnings (Should really just copy all the code to rmusic-tui)

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //! - `termwiz`: Enables the [`From<&InputEvent>`](enum.Input.html#method.from) implementation for [`Input`].
 
 #![forbid(unsafe_code)]
-#![warn(missing_docs)]
+// #![warn(missing_docs)]
 #![warn(rustdoc::missing_crate_level_docs)]
 #![warn(rustdoc::unescaped_backticks)]
 mod file_explorer;
